f52ecda8f842f418244de95a2627fa8519e0f7b4
david <davidrichakerja@gmail.com>
2024-09-22 09:40:31 +0700

    added UNIQUE constraints

--- migrations/1726893753186_add-constraints-to-all-tables.js
+++ migrations/1726893753186_add-constraints-to-all-tables.js
@@ -9,9 +9,17 @@ exports.shorthands = undefined;
  * @returns {Promise<void> | void}
  */
 exports.up = (pgm) => {
+  // menambahkan constraint UNIQUE, kombinasi dari kolom playlist_id dan song_id
+  // Guna menghindari nilai duplikasi dara antara keduanya
+  pgm.addConstraint('playlist_songs', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)');
+
+  // menambahkan constraint UNIQUE, kombinasi dari kolom note_id dan user_id
+  // Guna menghindari nilai duplikasi dara antara keduanya
+  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
+
   pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlists.id', 'FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE')
   pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE')
-  pgm.addConstraint('songs','fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE')
+  //pgm.addConstraint('songs','fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE')
   pgm.addConstraint('playlist_songs', 'fk_playlist_songs.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE')
   pgm.addConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE')
   pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE' )
